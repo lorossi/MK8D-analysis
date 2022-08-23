2669bc7979501f44053e616c2e33ae72bafc6fe5
lorossi <mail@lorenzoros.si>
2022-08-23 18:34:42 +0200

    added tool to automatically write into sql table

--- create_builds.py
+++ create_builds.py
@@ -1,4 +1,4 @@
-from database import MK8Deluxe
+from database import Database, MK8Deluxe
 from entities import Entity
 
 
@@ -21,9 +21,24 @@ def write_to_file(builds: list[Entity], path: str):
         f.writelines(b.toCSV() + "\n" for b in builds)
 
 
+def write_to_sql(builds: list[Entity], path: str):
+    d = Database("MK8D")
+
+    cols = builds[0].getCols()
+
+    d.deleteTable("builds")
+    d.createTable("builds", cols)
+
+    for b in builds:
+        d.insert("builds", cols, b.getRow())
+
+    d.applyChanges()
+
+
 def main():
     builds = build()
     write_to_file(builds, "builds.csv")
+    write_to_sql(builds, "builds.sql")
 
 
 if __name__ == "__main__":
