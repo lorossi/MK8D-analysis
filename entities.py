6eadcd0937d36df9db6de27135bb12a3dcd234a5
lorossi <mail@lorenzoros.si>
2022-08-23 17:22:25 +0200

    code cleaned, created part factory

--- entities.py
+++ entities.py
@@ -1,14 +1,15 @@
-from constants import EntityId, VARIABLES, IDS
+from statistics import stdev
+from constants import EntityId, PARTS_ATTRIBUTES, ID_ATTRIBUTES
 
 
 class Entity:
     def __init__(self, entity_id: EntityId, **kwargs) -> None:
         self._entity_id = entity_id
 
-        for v in VARIABLES:
+        for v in PARTS_ATTRIBUTES:
             self.__setattr__(v, kwargs.get(v, 0))
 
-        for i in IDS:
+        for i in ID_ATTRIBUTES:
             self.__setattr__(i, kwargs.get(i, None))
 
     def __str__(self) -> str:
@@ -20,9 +21,7 @@ class Entity:
         return self.__str__()
 
     def __add__(self, other: object):
-        if all(
-            not isinstance(other, c) for c in [Driver, Vehicle, Tyre, Glider, Build]
-        ):
+        if not isinstance(other, Entity):
             raise TypeError(
                 f"unsupported operand type(s) for +: "
                 f"'{self.__class__.__name__}' and '{type(other)}'"
@@ -32,7 +31,9 @@ class Entity:
         return Build(**kwargs)
 
     def _addAttributes(self, other: object) -> dict[str, int | str]:
-        common = set(self.__dict__.keys()) & set(other.__dict__.keys()) - set(IDS)
+        common = set(self.__dict__.keys()) & set(other.__dict__.keys()) - set(
+            ID_ATTRIBUTES
+        )
 
         kwargs = {}
 
@@ -44,7 +45,7 @@ class Entity:
             ):
                 kwargs[k] = self.__dict__[k] + " - " + other.__dict__[k]
 
-        for i in IDS:
+        for i in ID_ATTRIBUTES:
             if self.__getattribute__(i) is None:
                 if other.__getattribute__(i) is not None:
                     kwargs[i] = other.__getattribute__(i)
@@ -62,27 +63,41 @@ class Entity:
         return ",".join(str(k) for k in self.__dict__.keys() if not k.startswith("_"))
 
 
-class Driver(Entity):
-    def __init__(self, **kwargs: dict) -> None:
-        super().__init__(entity_id=EntityId.DRIVER, **kwargs)
-        self.size = kwargs.get("size")
+class Part(Entity):
+    def __init__(self, entity_id: EntityId, **kwargs) -> None:
+        super().__init__(entity_id, **kwargs)
 
 
-class Vehicle(Entity):
-    def __init__(self, **kwargs: dict) -> None:
-        super().__init__(entity_id=EntityId.VEHICLE, **kwargs)
+class PartFactory:
+    def __init__(self, entity_id: EntityId) -> None:
+        self._entity_id = entity_id
 
-
-class Tyre(Entity):
-    def __init__(self, **kwargs: dict) -> None:
-        super().__init__(entity_id=EntityId.TYRE, **kwargs)
-
-
-class Glider(Entity):
-    def __init__(self, **kwargs: dict) -> None:
-        super().__init__(entity_id=EntityId.GLIDER, **kwargs)
+    def build(self, **kwargs) -> Entity:
+        return Part(self._entity_id, **kwargs)
 
 
 class Build(Entity):
     def __init__(self, **kwargs: dict) -> None:
-        super().__init__(entity_id=EntityId.BUILD, **kwargs)
+        super().__init__(entity_id=None, **kwargs)
+
+
+class NamedBuild:
+    def __init__(self, **kwargs: dict) -> None:
+        if any(k.endswith("_id") for k in kwargs.keys()):
+            raise ValueError("NamedBuild cannot have an id")
+
+        for k, v in kwargs.items():
+            self.__setattr__(k, v)
+
+    def __str__(self) -> str:
+        return f"('score': {self.score}), ('stdev': {self.score_dev}), " + ", ".join(
+            str(a) for a in self.__dict__.items()
+        )
+
+    @property
+    def score(self) -> float:
+        return (self.speed + self.acceleration) / 2
+
+    @property
+    def score_dev(self) -> float:
+        return stdev([self.speed, self.acceleration])
