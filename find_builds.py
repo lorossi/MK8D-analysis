2de16d43913b89ba069d62a0afaa628dcf67da32
lorossi <mail@lorenzoros.si>
2022-08-23 17:22:45 +0200

    created script to find builds

--- find_builds.py
+++ find_builds.py
@@ -3,7 +3,18 @@ from database import MK8DeluxeBuilds
 
 def main():
     m = MK8DeluxeBuilds()
-    for b in m.builds:
+    m.sort_score = -1
+    m.sort_std_dev = 1
+    m.sort_miniturbo = -1
+
+    m.limit = 30
+    m.min_speed = 12
+    m.min_acceleration = 12
+
+    # print(m.available_filters)
+    # print(m.available_sorts)
+
+    for b in m.named_builds:
         print(b)
 
 
