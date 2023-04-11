c2d312d52e1de79d905d2d682d0c36cb52ac86b8
lorossi <mail@lorenzoros.si>
2023-04-11 15:42:21 +0200

    added whole lots of commands

--- find_builds.py
+++ find_builds.py
@@ -3,82 +3,183 @@ as they are saved in the database.
 
 The class offers the ability to sort, filter and score each of them.
 """
-from sys import argv
+import argparse
+
+
 from modules.database import MK8DeluxeBuilds
+from modules.builds_printer import BuildsPrinter
+from modules.command_parsers import (
+    FilterParser,
+    SortParser,
+    WeightParser,
+)
 
 
-def gather_parameters(argv: list) -> dict:
-    """Gather the parameters from the command line and return them as a dictionary."""
-    if "-h" in argv:
-        print(
-            "Usage: python3 find_builds.py "
-            "[--csv] [--json] [--json-pretty] [--markdown]"
-        )
-        return None
-
-    params = {}
-    params["csv"] = "--csv" in argv
-    params["json"] = "--json" in argv
-    params["json_pretty"] = "--json-pretty" in argv
-    params["markdown"] = "--markdown" in argv
-
-    if sum(1 for v in params.values() if v) > 1:
-        print("Only one format can be specified.")
-        return None
-
-    params["default"] = not any(params.values())
-
-    return params
-
-
-def main(parameters: dict[str, bool]) -> None:
+def find(parameters: argparse.Namespace) -> None:
     """Run the main function for the create named builds script."""
-    m = MK8DeluxeBuilds()
-    # use m.available_filters to get a list of available filters
-    # assign the value to the corresponding attribute of the MK8DeluxeBuilds object
-    m.min_ground_speed = 12
-    m.min_acceleration = 12
-    m.min_miniturbo = 5
 
-    # use m.available_sorts_order to get a list of available sort options
+    if parameters.list_filters:
+        print(MK8DeluxeBuilds.getAvailableFilters())
+        return
+    if parameters.list_sort_orders:
+        print(MK8DeluxeBuilds.getAvailableSortOrders())
+        return
+    if parameters.list_weights:
+        print(MK8DeluxeBuilds.getAvailableWeights())
+        return
+
+    m = MK8DeluxeBuilds()
+    # use m.available_filters or MK8DeluxeBuilds.getAvailableFilters() to get a
+    # list of available filters
+    # assign the value to the corresponding attribute of the MK8DeluxeBuilds object
+    if parameters.query_filters is not None:
+        for key, value in parameters.query_filters.items():
+            setattr(m, key, value)
+
+    # use m.available_sorts_order or MK8DeluxeBuilds.getAvailableSortOrders() to get a
+    # list of available sort options
     # assign the value to the corresponding attribute of the MK8DeluxeBuilds object
     # -1 to sort descending, 1 to sort ascending
-    m.sort_score = -1
-    m.sort_score_dev = 1
-    m.sort_miniturbo = -1
+    if parameters.query_sort is not None:
+        for key, value in parameters.query_sort.items():
+            setattr(m, key, value)
 
-    # the weights are used to calculate the score as a weighted average
-    #   of the parts attributes
-    # use m.available_weights to get a list of available weights options
-    # assign the value to the corresponding attribute of the MK8DeluxeBuilds object
-    # the default value is 0.5 for ground_speed and acceleration while
-    # all the other weights are 0
-    m.weight_ground_speed = 0.5
-    m.weight_acceleration = 0.4
-    m.weight_miniturbo = 0.1
+    if parameters.query_weights is not None:
+        m.weights = parameters.query_weights
 
     # use m.limit to set the maximum number of results to return
-    m.limit = 5
+    m.limit = parameters.limit
 
-    for x, b in enumerate(m.named_builds):
-        match parameters:
-            case {"csv": True}:
-                if x == 0:
-                    print(b.csvHeader())
-                print(b.csv)
-            case {"json": True}:
-                print(b.json)
-            case {"json_pretty": True}:
-                print(b.toJSON(indent=4))
-            case {"markdown": True}:
-                if x == 0:
-                    print(b.markdownHeader())
-                print(b.markdown)
-            case {"default": True}:
-                print(b)
+    # builds can be either be scored (and then accessed via m.scored_named_builds)
+    # or the best builds can be computed via the BNL algorithm (for skyline queries,
+    # check the readme for more info)
+
+    if parameters.dominating:
+        builds = m.dominating_named_builds
+    elif parameters.score:
+        builds = m.scored_named_builds
+
+    # use the BuildsPrinter class to print the builds
+    if parameters.csv:
+        BuildsPrinter.printCSV(builds)
+    elif parameters.json:
+        BuildsPrinter.printJSON(builds)
+    elif parameters.json_pretty:
+        BuildsPrinter.printJSONPretty(builds)
+    elif parameters.markdown:
+        BuildsPrinter.printMarkdown(builds)
+    else:
+        for x, b in enumerate(builds):
+            print(f"{x}: {b}")
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        "Find builds",
+        description="Find builds with the specified parameters.",
+    )
+
+    # parser group for listing available filters and sorters
+    list_parser = parser.add_mutually_exclusive_group(required=False)
+    list_parser.add_argument(
+        "--list-filters",
+        action="store_true",
+        help="List the available filters.",
+    )
+
+    list_parser.add_argument(
+        "--list-sort-orders",
+        action="store_true",
+        help="List the available sorters.",
+    )
+
+    list_parser.add_argument(
+        "--list-weights",
+        action="store_true",
+        help="List the available weights.",
+    )
+
+    # parser group for query options
+    query_parser = parser.add_argument_group("Query options")
+
+    # parser group for mode output
+    output_parser = query_parser.add_mutually_exclusive_group(required=False)
+    output_parser.add_argument(
+        "--csv",
+        action="store_true",
+        help="Print the builds in CSV format.",
+    )
+
+    output_parser.add_argument(
+        "--json",
+        action="store_true",
+        help="Print the builds in JSON format.",
+    )
+
+    output_parser.add_argument(
+        "--json-pretty",
+        action="store_true",
+        help="Print the builds in JSON format with indentation.",
+    )
+
+    output_parser.add_argument(
+        "--markdown",
+        action="store_true",
+        help="Print the builds in Markdown format.",
+    )
+
+    # parser group for query algorithm
+    algorithm_parser = query_parser.add_mutually_exclusive_group(required=False)
+    algorithm_parser.add_argument(
+        "--score",
+        action="store_true",
+        default=True,
+        help="Find the builds according to their scores (default).",
+    )
+
+    algorithm_parser.add_argument(
+        "--dominating",
+        action="store_true",
+        help="Find the builds according to the BNL algorithm.",
+    )
+
+    # parser group for query parameters
+    parameters_parser = query_parser.add_argument_group("Query parameters")
+    parameters_parser.add_argument(
+        "--limit",
+        type=int,
+        default=5,
+        help="Limit the number of results to return.",
+    )
+
+    parameters_parser.add_argument(
+        "--query-filters",
+        dest="query_filters",
+        nargs="+",
+        help="Query filters to apply to the query.",
+        action=FilterParser,
+    )
+
+    parameters_parser.add_argument(
+        "--query-sorters",
+        dest="query_sort",
+        nargs="+",
+        help="Query sorters to apply to the query.",
+        action=SortParser,
+    )
+
+    parameters_parser.add_argument(
+        "--query-weights",
+        dest="query_weights",
+        nargs="+",
+        help="Query weights to apply to the query.",
+        action=WeightParser,
+    )
+
+    args = parser.parse_args()
+
+    find(args)
 
 
 if __name__ == "__main__":
-    parameters = gather_parameters(argv)
-    if parameters:
-        main(parameters)
+    main()
