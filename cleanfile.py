d6755c22aa472341eb1cdecc99d17837aeccc584
lorossi <mail@lorenzoros.si>
2023-04-11 10:40:55 +0200

    updated script to merge together common entities

--- cleanfile.py
+++ cleanfile.py
@@ -1,5 +1,4 @@
 import re
-
 from glob import glob
 
 
@@ -17,12 +16,12 @@ def open_file(path: str) -> list:
 def clean_file(lines: list) -> list:
     clean_lines = []
 
-    lines = [line for line in lines if "from Mario Kart 8" not in line]
+    lines = [line for line in lines if "Mario Kart 8" not in line]
     for x in range(len(lines)):
         if not re.match(r".+\s+\d+", lines[x]):
             continue
 
-        clean_lines.append(re.sub(r"(\S+)(\s{2,})", r"\1, ", lines[x]))
+        clean_lines.append(re.sub(r"(\S+)(\s{2,})", r"\1,", lines[x]))
     return clean_lines
 
 
@@ -33,10 +32,43 @@ def clean_drivers(lines: list) -> list:
         name = lines[x]
         stats = re.sub(r"(\w+)(.*)", r"\2", lines[x + 1])
         new_line = f"{name} {stats}"
-        csv_line = re.sub(r"(\S+)(\s{2,})", r"\1, ", new_line)
+        csv_line = re.sub(r"(\S+)(\s{2,})", r"\1,", new_line)
         clean_lines.append(csv_line)
 
     return clean_lines
+
+
+def extract_stats(line: str) -> tuple:
+    name, *stats = line.split(",")
+    return name, ",".join(stats)
+
+
+def merge_lines(lines: list) -> list:
+    merged = dict()
+
+    for line in lines[1:]:
+        name, stats = extract_stats(line)
+
+        if stats in merged:
+            merged[stats].append(name)
+        else:
+            merged[stats] = [name]
+
+    ids = []
+    merged_lines = []
+    current_id = 0
+    for stats, names in merged.items():
+        current_id += 1
+        merged_lines.append(f"{current_id},{stats}")
+
+        for name in names:
+            ids.append(f"{name},{current_id}")
+
+    merged_lines.sort()
+    new_header = f"id, {','.join(lines[0].split(',')[1:])}"
+    merged_lines.insert(0, new_header)
+
+    return merged_lines, ids
 
 
 def write_file(path: str, lines: list) -> None:
@@ -45,12 +77,24 @@ def write_file(path: str, lines: list) -> None:
             f.write(line + "\n")
 
 
-def clean(path: str, out_path: str) -> str:
+def create_path(path: str, folder: str, suffix: str = None) -> str:
+    folders = path.split("/")[:-1]
+    filename = path.split("/")[-1]
+
+    if suffix:
+        return (
+            f"{'/'.join(folders[:-1])}/{folder}/{filename.split('.')[0]}_{suffix}.csv"
+        )
+
+    return f"{'/'.join(folders[:-1])}/{folder}/{filename.split('.')[0]}.csv"
+
+
+def clean(path: str) -> str:
     lines = open_file(path)
     clean_lines = []
 
     # replace multiple spaces with a comma
-    header = re.sub(r"(\S+)(\s{2,})", r"\1, ", lines[0])
+    header = re.sub(r"(\S+)(\s{2,})", r"\1,", lines[0])
     clean_lines.append(header)
 
     if "driver" in path.lower():
@@ -58,19 +102,23 @@ def clean(path: str, out_path: str) -> str:
     else:
         clean_lines.extend(clean_file(lines[1:]))
 
-    filename = path.split("/")[-1]
-    out_path = f"{out_path}/{filename.split('.')[0]}.csv"
+    out_path = create_path(path, "clean")
     write_file(out_path, clean_lines)
+
+    merged, ids = merge_lines(clean_lines)
+    merged_path = create_path(path, "merged")
+    write_file(merged_path, merged)
+    ids_path = create_path(path, "merged", "ids")
+    write_file(ids_path, ids)
 
     return out_path
 
 
 def main() -> None:
     dirty_path = "data/dirty/*.csv"
-    out_path = "data/clean"
     for path in glob(dirty_path):
-        clean_path = clean(path, out_path)
-        print(f"Cleaned {path} to {clean_path}")
+        clean(path)
+        print(f"Cleaned {path}")
 
 
 if __name__ == "__main__":
